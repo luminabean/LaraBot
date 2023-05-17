0007f41ea01e097eae5fc772d32e8e2fee7feecc
빛콩 <38746279+luminabean@users.noreply.github.com>
2023-05-17 11:33:59 +0900

    Ver 1.0.7
    - 시드 정보가 제대로 출력되지 않았던 오류를 수정

--- Cogs/Dojo.py
+++ Cogs/Dojo.py
@@ -105,72 +105,72 @@ class Dojo(commands.Cog, name="무릉도장"):
                 if identifier != []:
                     print("무릉 기록이 없습니다.")
                     await ctx.send(nickname + '님은 무릉을 친 적이 없는 것 같아요!')
-
-                # 정보 크롤링
-                world = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-8 > div > h3 > img')['alt']
-                job = soup.select_one(
-                    '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.user-summary > ul > li:nth-child(2)').get_text()
-                guild = soup.select_one(
-                    '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.row.row-normal.user-additional > div.col-lg-2.col-md-4.col-sm-4.col-12')
-                guild = guild.get_text().split()
-                guild = guild[1]
-                character_img_url = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-4.pt-1.pt-sm-0.pb-1.pb-sm-0.text-center.mt-2.mt-lg-0 > div > div.col-6.col-md-8.col-lg-6 > img')['src']
-                urllib.request.urlretrieve(character_img_url, "character.png")
-                floor = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > div > div > div > h1').get_text().replace('\n', '')
-                time = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > div > div > div > small').get_text()
-                date = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > footer > div.user-summary-date > span').get_text()
-
-                # 출력 테스트
-                print(f"월드: {world}")
-                print(f"층수: {floor}")
-                print(f"시간: {time}")
-                print(f"{date}")
-
-                # 임베드 변수
-                embed_title = nickname
-                embed_description = world + " / " + job
-                char_img = discord.File("character.png", filename="char.png")
-
-                # 임베드 양식
-                # 제작자 캐릭터면 루미나 그린색
-                if nickname == "탠루나" or nickname == "나방콩":
-                    embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
-                # 리부트는 청록색
-                elif world == "리부트" or world == "리부트2":
-                    # 제작자가 속한 길드의 유저라면 밝은 회색
-                    if guild == "그리폰":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.light_gray())
-                    # 제작자 1인 길드 소속 캐릭터면 루미나 그린색
-                    elif guild == "심포니":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
-                    else:
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.teal())
-                # 일반섭은 주황색
                 else:
-                    # 제작자 1인 길드 소속 캐릭터면 루미나 옐로우색
-                    if guild == "루미나" and world == "크로아":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0xE2E276)
+                    # 정보 크롤링
+                    world = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-8 > div > h3 > img')['alt']
+                    job = soup.select_one(
+                        '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.user-summary > ul > li:nth-child(2)').get_text()
+                    guild = soup.select_one(
+                        '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.row.row-normal.user-additional > div.col-lg-2.col-md-4.col-sm-4.col-12')
+                    guild = guild.get_text().split()
+                    guild = guild[1]
+                    character_img_url = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-4.pt-1.pt-sm-0.pb-1.pb-sm-0.text-center.mt-2.mt-lg-0 > div > div.col-6.col-md-8.col-lg-6 > img')['src']
+                    urllib.request.urlretrieve(character_img_url, "character.png")
+                    floor = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > div > div > div > h1').get_text().replace('\n', '')
+                    time = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > div > div > div > small').get_text()
+                    date = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(1) > section > footer > div.user-summary-date > span').get_text()
+
+                    # 출력 테스트
+                    print(f"월드: {world}")
+                    print(f"층수: {floor}")
+                    print(f"시간: {time}")
+                    print(f"{date}")
+
+                    # 임베드 변수
+                    embed_title = nickname
+                    embed_description = world + " / " + job
+                    char_img = discord.File("character.png", filename="char.png")
+
+                    # 임베드 양식
+                    # 제작자 캐릭터면 루미나 그린색
+                    if nickname == "탠루나" or nickname == "나방콩":
+                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
+                    # 리부트는 청록색
+                    elif world == "리부트" or world == "리부트2":
+                        # 제작자가 속한 길드의 유저라면 밝은 회색
+                        if guild == "그리폰":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.light_gray())
+                        # 제작자 1인 길드 소속 캐릭터면 루미나 그린색
+                        elif guild == "심포니":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
+                        else:
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.teal())
+                    # 일반섭은 주황색
                     else:
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=discord.Color.orange())
-                embed.set_thumbnail(url="attachment://char.png")
-                embed.add_field(name="", value="", inline=False)
-                embed.add_field(name="달성 층", value=floor, inline=True)
-                embed.add_field(name="", value="", inline=True)
-                embed.add_field(name="소요 시간", value=time, inline=True)
-                embed.add_field(name="", value="", inline=False)
+                        # 제작자 1인 길드 소속 캐릭터면 루미나 옐로우색
+                        if guild == "루미나" and world == "크로아":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0xE2E276)
+                        else:
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=discord.Color.orange())
+                    embed.set_thumbnail(url="attachment://char.png")
+                    embed.add_field(name="", value="", inline=False)
+                    embed.add_field(name="달성 층", value=floor, inline=True)
+                    embed.add_field(name="", value="", inline=True)
+                    embed.add_field(name="소요 시간", value=time, inline=True)
+                    embed.add_field(name="", value="", inline=False)
 
-                if int(floor[:-1]) in party_boss.keys() and int(floor[:-1]) in solo_boss.keys():
-                    embed.add_field(name="추천 보스", value="파티격: " + party_boss[int(floor[:-1])] + "\n" + "솔로격: " + solo_boss[int(floor[:-1])])
-                elif int(floor[:-1]) in party_boss.keys():
-                    embed.add_field(name="추천 보스", value="파티격: " + party_boss[int(floor[:-1])] + "\n")
-                elif int(floor[:-1]) in solo_boss.keys():
-                    embed.add_field(name="추천 보스", value="솔로격: " + solo_boss[int(floor[:-1])] + "\n")
-                embed.add_field(name="", value="", inline=False)
-                embed.set_footer(text=date + "\n추천 보스 정보는 정확하지 않을 수도 있습니다.")
+                    if int(floor[:-1]) in party_boss.keys() and int(floor[:-1]) in solo_boss.keys():
+                        embed.add_field(name="추천 보스", value="파티격: " + party_boss[int(floor[:-1])] + "\n" + "솔로격: " + solo_boss[int(floor[:-1])])
+                    elif int(floor[:-1]) in party_boss.keys():
+                        embed.add_field(name="추천 보스", value="파티격: " + party_boss[int(floor[:-1])] + "\n")
+                    elif int(floor[:-1]) in solo_boss.keys():
+                        embed.add_field(name="추천 보스", value="솔로격: " + solo_boss[int(floor[:-1])] + "\n")
+                    embed.add_field(name="", value="", inline=False)
+                    embed.set_footer(text=date + "\n추천 보스 정보는 정확하지 않을 수도 있습니다.")
 
 
-                await ctx.send(nickname + '님의 무릉도장 최고기록 정보를 불러왔어요!')
-                await ctx.channel.send(embed=embed, file=char_img)
+                    await ctx.send(nickname + '님의 무릉도장 최고기록 정보를 불러왔어요!')
+                    await ctx.channel.send(embed=embed, file=char_img)
         else:
             print(response.status_code)
 
--- Cogs/Union.py
+++ Cogs/Union.py
@@ -48,67 +48,67 @@ class Union(commands.Cog, name="유니온"):
                 if identifier != []:
                     print("무릉 기록이 없습니다.")
                     await ctx.send(nickname + '님은 본캐가 아닌 것 같아요!')
-
-                # 정보 크롤링
-                world = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-8 > div > h3 > img')['alt']
-                guild = soup.select_one(
-                    '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.row.row-normal.user-additional > div.col-lg-2.col-md-4.col-sm-4.col-12')
-                guild = guild.get_text().split()
-                guild = guild[1]
-                rank_img_url = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > img')['src']
-                urllib.request.urlretrieve("https:" + rank_img_url, "union_rank.png")
-                rank_name = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > div').get_text()
-                union_level = soup.select_one(
-                    '#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > span').get_text()
-                power = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > footer > div.d-block.mb-1 > span').get_text().split()[1]
-                date = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > footer > div.user-summary-date > span').get_text()
-
-                # 출력 테스트
-                print(f"월드: {world}")
-                print(f"랭크: {rank_name}")
-                print(f"유니온 레벨: {union_level[3:]}")
-                print(f"전투력: {power}")
-                print(f"{date}")
-
-                # 임베드 변수
-                embed_title = rank_name
-                embed_description = nickname + " / " + world
-                rank_img = discord.File("union_rank.png", filename="rank.png")
-                daily_coin = round((int(power.replace(",","")) * 86400) / 100000000000, 2)
-
-                # 임베드 양식
-                # 제작자 캐릭터면 루미나 그린색
-                if nickname == "탠루나" or nickname == "나방콩":
-                    embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
-                # 리부트는 청록색
-                elif world == "리부트" or world == "리부트2":
-                    # 제작자가 속한 길드의 유저라면 밝은 회색
-                    if guild == "그리폰":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.light_gray())
-                    # 제작자 1인 길드 소속 캐릭터면 루미나 그린색
-                    elif guild == "심포니":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
-                    else:
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.teal())
-                # 일반섭은 주황색
                 else:
-                    # 제작자 1인 길드 소속 캐릭터면 루미나 옐로우색
-                    if guild == "루미나" and world == "크로아":
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0xE2E276)
+                    # 정보 크롤링
+                    world = soup.select_one('#user-profile > section > div.row.row-normal > div.col-lg-8 > div > h3 > img')['alt']
+                    guild = soup.select_one(
+                        '#user-profile > section > div.row.row-normal > div.col-lg-8 > div > div.row.row-normal.user-additional > div.col-lg-2.col-md-4.col-sm-4.col-12')
+                    guild = guild.get_text().split()
+                    guild = guild[1]
+                    rank_img_url = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > img')['src']
+                    urllib.request.urlretrieve("https:" + rank_img_url, "union_rank.png")
+                    rank_name = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > div').get_text()
+                    union_level = soup.select_one(
+                        '#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > div > div > span').get_text()
+                    power = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > footer > div.d-block.mb-1 > span').get_text().split()[1]
+                    date = soup.select_one('#app > div.card.border-bottom-0 > div > section > div.row.text-center > div:nth-child(3) > section > footer > div.user-summary-date > span').get_text()
+
+                    # 출력 테스트
+                    print(f"월드: {world}")
+                    print(f"랭크: {rank_name}")
+                    print(f"유니온 레벨: {union_level[3:]}")
+                    print(f"전투력: {power}")
+                    print(f"{date}")
+
+                    # 임베드 변수
+                    embed_title = rank_name
+                    embed_description = nickname + " / " + world
+                    rank_img = discord.File("union_rank.png", filename="rank.png")
+                    daily_coin = round((int(power.replace(",","")) * 86400) / 100000000000, 2)
+
+                    # 임베드 양식
+                    # 제작자 캐릭터면 루미나 그린색
+                    if nickname == "탠루나" or nickname == "나방콩":
+                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
+                    # 리부트는 청록색
+                    elif world == "리부트" or world == "리부트2":
+                        # 제작자가 속한 길드의 유저라면 밝은 회색
+                        if guild == "그리폰":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.light_gray())
+                        # 제작자 1인 길드 소속 캐릭터면 루미나 그린색
+                        elif guild == "심포니":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0x99E593)
+                        else:
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description,color=discord.Color.teal())
+                    # 일반섭은 주황색
                     else:
-                        embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=discord.Color.orange())
-                embed.set_thumbnail(url="attachment://rank.png")
-                embed.add_field(name="", value="", inline=False)
-                embed.add_field(name="유니온 레벨", value=union_level[3:], inline=False)
-                embed.add_field(name="전투력", value=power, inline=True)
-                embed.add_field(name="", value="", inline=True)
-                embed.add_field(name="일간 획득 코인량", value=str(daily_coin)+"개", inline=True)
-                embed.add_field(name="", value="")
-                embed.set_footer(text=date)
+                        # 제작자 1인 길드 소속 캐릭터면 루미나 옐로우색
+                        if guild == "루미나" and world == "크로아":
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=0xE2E276)
+                        else:
+                            embed = discord.Embed(title=embed_title, url=url, description=embed_description, color=discord.Color.orange())
+                    embed.set_thumbnail(url="attachment://rank.png")
+                    embed.add_field(name="", value="", inline=False)
+                    embed.add_field(name="유니온 레벨", value=union_level[3:], inline=False)
+                    embed.add_field(name="전투력", value=power, inline=True)
+                    embed.add_field(name="", value="", inline=True)
+                    embed.add_field(name="일간 획득 코인량", value=str(daily_coin)+"개", inline=True)
+                    embed.add_field(name="", value="")
+                    embed.set_footer(text=date)
 
 
-                await ctx.send(nickname + '님의 유니온 정보를 불러왔어요!')
-                await ctx.channel.send(embed=embed, file=rank_img)
+                    await ctx.send(nickname + '님의 유니온 정보를 불러왔어요!')
+                    await ctx.channel.send(embed=embed, file=rank_img)
         else:
             print(response.status_code)
 
